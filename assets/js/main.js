5d398de45ab70022eff851e005e64e6ca03d78bb
freetimezp <kraftukrnet@ukr.net>
2025-03-09 20:59:26 +0200

    03. usefull-things-061-fullscreen-menu. GSAP animation.

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+let tl = new TimelineMax({ paused: true });
+
+tl.to(".nav-container", 1, {
+    left: 0,
+    ease: Expo.easeInOut
+});
+
+tl.staggerFrom(".menu > div", 0.8, {
+    y: 100,
+    opacity: 0,
+    ease: Expo.easeInOut
+}, "0.1", "-=0.4");
+
+tl.staggerFrom(".socials", 0.8, {
+    y: 100,
+    opacity: 0,
+    ease: Expo.easeOut
+}, "0.4", "-=0.6");
+
+tl.reverse();
+
+$(document).on("click", ".menu-open", function () {
+    tl.reversed(!tl.reversed());
+});
+$(document).on("click", ".menu-close", function () {
+    tl.reversed(!tl.reversed());
+});
+
+
+
+
+
+
+
+
+
